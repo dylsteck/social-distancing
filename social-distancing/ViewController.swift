4bd77d338504d2a3eb9bd315f07426e5c0e77263
Dylan Steck <dylancsteck@gmail.com>
2020-03-28 19:48:51 -0400

    Initial Commit

---
+++ social-distancing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  social-distancing
+//
+//  Created by Dylan Steck on 3/28/20.
+//  Copyright © 2020 DylanSteck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
